e13d39c93dbefa896c613a425bb4249e8037a8c5
PieterSmith81 <109296884+PieterSmith81@users.noreply.github.com>
2025-12-02 15:01:29 +0000

    gitignore updated

---
+++ 002. Language Guide/002. Module 2 - Memory and Data Semantics/001. Variables/001. Exercise 1/exercise1.go
@@ -0,0 +1,44 @@
+// Declare three variables that are initialized to their zero value and three
+// declared with a literal value. Declare variables of type string, int and
+// bool. Display the values of those variables.
+//
+// Declare a new variable of type float32 and initialize the variable by
+// converting the literal value of Pi (3.14).
+package main
+
+import "fmt"
+
+// Add imports
+
+// main is the entry point for the application.
+func main() {
+
+	// Declare variables that are set to their zero value.
+	var s string
+	var b bool
+	var i int
+
+	// Display the value of those variables.
+	fmt.Println(s)
+	fmt.Println(b)
+	fmt.Println(i)
+	fmt.Println()
+
+	// Declare variables and initialize.
+	// Using the short variable declaration operator.
+	ss := "Hey, Ho, Let's Go!"
+	bb := false
+	ii := 42
+
+	// Display the value of those variables.
+	fmt.Println(ss)
+	fmt.Println(bb)
+	fmt.Println(ii)
+	fmt.Println()
+
+	// Perform a type conversion.
+	pi := float32(3.14)
+
+	// Display the value of that variable.
+	fmt.Println(pi)
+}
---
+++ 002. Language Guide/README.md
@@ -0,0 +1,10 @@
+# Ardan Labs Ultimate Go - Language Guide
+
+## My Solutions
+
+My solutions, demos, and example code from the Ardan Labs Ultimate Go - Language Guide course.
+
+## Course GitHub Repository
+
+The original GitHub repository for this course (including some great Go-related links) can be found at the following location:  
+https://github.com/ardanlabs/gotraining
