edfb17130a7701d1100d9e6714ddfd958a481185
PieterSmith81 <109296884+PieterSmith81@users.noreply.github.com>
2025-12-05 11:44:10 +0000

    Constants code added

---
+++ 002. Language Guide/002. Module 2 - Memory and Data Semantics/004. Constants/exercise1.go
@@ -0,0 +1,30 @@
+// Declare an untyped and typed constant and display their values.
+//
+// Multiply two literal constants into a typed variable and display the value.
+package main
+
+// Add imports.
+import "fmt"
+
+const (
+	// Declare a constant named server of kind string and assign a value.
+	server = "server001"
+
+	// Declare a constant named port of type integer and assign a value.
+	port = 443
+)
+
+func main() {
+
+	// Display the value of both server and port.
+	fmt.Printf("Server (type %T):\t%v\n", server, server)
+	fmt.Printf("Port (type %T):\t%v\n", port, port)
+	fmt.Println()
+
+	// Divide a constant of kind integer and kind floating point and
+	// assign the result to a variable.
+	result := 1 / 3.0
+
+	// Display the value of the variable.
+	fmt.Printf("Result (type %T):\t%v\n", result, result)
+}
